92da81854b63bf266b34105a69773a98db4d5382
yollet <yollet@eduhuake.com>
2019-04-25 09:04:20 +0800

    Initial Commit

---
+++ YOTool/YOTool.h
@@ -0,0 +1,19 @@
+//
+//  YOTool.h
+//  YOTool
+//
+//  Created by jhj on 2019/4/25.
+//  Copyright © 2019 jhj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YOTool.
+FOUNDATION_EXPORT double YOToolVersionNumber;
+
+//! Project version string for YOTool.
+FOUNDATION_EXPORT const unsigned char YOToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YOTool/PublicHeader.h>
+
+
---
+++ YOToolTests/YOToolTests.m
@@ -0,0 +1,37 @@
+//
+//  YOToolTests.m
+//  YOToolTests
+//
+//  Created by jhj on 2019/4/25.
+//  Copyright © 2019 jhj. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YOToolTests : XCTestCase
+
+@end
+
+@implementation YOToolTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
